7bdfa4378cd9630d96b6c19b12810a9d92a762eb
Sunny Yoo <isunyoo@gmail.com>
2021-06-02 20:52:48 +0800

    Updated for bug fixes

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for                          
+from flask import Flask, render_template, redirect, url_for, stream_with_context                       
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
@@ -16,10 +16,12 @@ app.config['SECRET_KEY'] = 'abcd1234$'
 def home() :
     form = PowerState()
     buttonStatus = False
+    streamData = ''
 
     if form.validate_on_submit() :
         if form.state.label.text == 'OFF' :
             buttonStatus = False
+            buttonStatus = stream()
             print(buttonStatus)
             PowerState.state = SubmitField('ON')            
             # def generate():
@@ -34,15 +36,17 @@ def home() :
             PowerState.state = SubmitField('OFF')            
 
         # return redirect(url_for('home'))
-    return render_template('demo.html', value0=buttonStatus, form=form)
+    return render_template('demo.html', value0=buttonStatus, value1=streamData, form=form)
 
-# def stream():
-#     def generate():
-#         with open('/home/syoo/.ethereum/importedKey/importedPrivateKeys') as f:
-#             while True:
-#                 yield f.read()                                
-#                 sleep(1)        
-#     return app.response_class(generate(), mimetype='text/plain')
+def stream():
+    def generate():
+        with open('/home/syoo/.ethereum/importedKey/importedPrivateKeys') as f:
+            while True:
+                yield f.read()                                
+                sleep(1)        
+    # return app.response_class(generate(), mimetype='text/plain')
+    return app.response_class(stream_with_context(generate()))
+
 
 @app.route('/stream')
 def stream():
