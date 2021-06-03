372d9e383ca73af92a4a5ecd473e31ca22eba319
isunyoo <isunyoo@gmail.com>
2021-06-03 13:48:28 +0800

    Updated for bug fixes

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for                          
+from flask import Flask, render_template, redirect, url_for, stream_with_context, Response
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
@@ -16,6 +16,7 @@ app.config['SECRET_KEY'] = 'abcd1234$'
 def home() :
     form = PowerState()
     buttonStatus = False
+    streamDaata = ''
 
     if form.validate_on_submit() :
         if form.state.label.text == 'OFF' :
@@ -27,31 +28,34 @@ def home() :
             #         while True:
             #             yield f.read()                                
             #             sleep(1)        
-            # return app.response_class(generate(), mimetype='text/plain')                     
+            # return app.response_class(generate(), mimetype='text/plain')                              
         elif form.state.label.text == 'ON' :
             buttonStatus = True
+            streamDaata = stream()  
             print(buttonStatus)                                    
             PowerState.state = SubmitField('OFF')            
 
         # return redirect(url_for('home'))
-    return render_template('demo.html', value0=buttonStatus, form=form)
+    return render_template('demo.html', value0=buttonStatus, value1=streamDaata, form=form)
 
-# def stream():
-#     def generate():
-#         with open('/home/syoo/.ethereum/importedKey/importedPrivateKeys') as f:
-#             while True:
-#                 yield f.read()                                
-#                 sleep(1)        
-#     return app.response_class(generate(), mimetype='text/plain')
+def stream():
+    def generate():
+        with open('/home/syoo/.ethereum/development/importedKey/importedPrivateKeys') as f:
+            while True:
+                yield f.read()                                
+                sleep(1)        
+    # return app.response_class(generate(), mimetype='text/plain')    
+    return Response(stream_with_context(generate()))
 
 @app.route('/stream')
 def stream():
     def generate():
-        with open('/home/syoo/.ethereum/importedKey/importedPrivateKeys') as f:
+        with open('/home/syoo/.ethereum/development/importedKey/importedPrivateKeys') as f:
             while True:
                 yield f.read()                                
                 sleep(1)        
 
-    return app.response_class(generate(), mimetype='text/plain')
+    # return app.response_class(generate(), mimetype='text/plain')
+    return Response(stream_with_context(generate()))
 
 app.run()
